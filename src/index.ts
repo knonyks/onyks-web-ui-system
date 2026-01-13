9934d44d98cd9982ae28b5c3de66525b600bed7d
zero-jedynkowy <01110010pl@gmail.com>
2026-01-13 05:28:32 +0100

    Fix export of the components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './components/button.ts';
 export * from './components/textfield.ts';
+export * from './components/logo.ts';
+export * from './components/table.ts';
+export * from './components/label.ts';
